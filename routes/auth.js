25a80c0b2feb9842c06cfe96eba14bcc9d35ef13
kevinbird61 <kevinbird61@gmail.com>
2018-04-15 01:05:58 +0800

    Last updated.
    Prepare to move this project to gitlab.

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,11 @@ const router = new express.Router();
 // user login - ulogin
 router.post('/ulogin',function(req,res){
     // Get the information from req body
-    console.log(req.body.username);
+    console.log(`Username: ${req.body.username}, Passwd: ${req.body.passwd}`);
+    // TODO: find the matching product_key from user account
     res.end(JSON.stringify({
-        msg: "OK"
+        msg: "OK",
+        product_key: "123456789"
     }));
 })
 
